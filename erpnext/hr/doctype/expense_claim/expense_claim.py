fe4a7b7eebffeed9ffe36ee631a04131a00cf856
Khoa Nguyen <nak1090@gmail.com>
2020-03-24 16:11:37 +0700

    update expense_claim validate_sanctioned_amount

--- erpnext/hr/doctype/expense_claim/expense_claim.py
+++ erpnext/hr/doctype/expense_claim/expense_claim.py
@@ -250,7 +250,7 @@ class ExpenseClaim(AccountsController):
 
 	def validate_sanctioned_amount(self):
 		for d in self.get('expenses'):
-			if flt(d.sanctioned_amount) > flt(d.amount):
+			if flt(d.sanctioned_amount) < 0:
 				frappe.throw(_("Sanctioned Amount cannot be less than 0."))
 
 	def set_expense_account(self, validate=False):
